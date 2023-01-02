3f02a1665aa9ebd92b034ab19fb43c2b46ea8be5
minmi <sso02027@naver.com>
2023-01-02 17:31:37 +0900

    route 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import {BrowserRouter,Navigate,Route,Routes} from 'react-router-dom';
 import Start from './pages/Start';
+import Test from './pages/Test';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path = '/' element={<Start/>}></Route>
+          <Route path = '/test' element={<Test/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
